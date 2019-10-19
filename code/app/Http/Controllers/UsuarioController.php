ae77bfe4a608cea5e120070b96de970233f404a0
Programador GMM <programadorgmm@gmail.com>
2019-10-19 12:17:51 -0300

    Criar metodo index do Usuario
    - Criar controllers de todas as tabelas
    - Criar model Usuario
    - Criar estrutura do UsuarioController
    - Criar metodo index do UsuarioController

---
+++ code/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Usuario;
+use Illuminate\Http\Request;
+use function MongoDB\BSON\toJSON;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Usuario::all()->toJson();
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $usuario = new Usuario();
+        $usuario->nome = $request->nome;
+        $usuario->email = $request->email;
+        $usuario->senha = $request->senha;
+        $usuario->save();
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ code/app/Usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    protected $fillable = ['nome','email','senha'];
+    protected $guarded = ['id', 'created_at', 'update_at'];
+    protected $table = 'usuario';
+}
